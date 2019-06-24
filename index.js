3c3919b75ded941bbf8675de397af70a8fe57480
Aaron Sikes <aaron@sikes.io>
2019-06-24 17:20:44 -0400

    Support "collections", instead of synchronizing only a global store

--- index.js
+++ index.js
@@ -12,12 +12,12 @@ var dbFile = './data/sqlite.db';
 var exists = fs.existsSync(dbFile);
 var db = new sqlite3.Database(dbFile);
 
-db.serialize(function(){
-  if (!exists) {
-      db.run('CREATE TABLE atoms (server_index integer primary key, client text, client_index integer, value text)');
-      db.run('CREATE UNIQUE INDEX unique_atoms on atoms(client, client_index)');
-  }
-});
+if (!exists) {
+  db.serialize(function(){
+      db.run('CREATE TABLE atoms (server_index integer primary key, collection text, client text, client_index integer, value text)');
+      db.run('CREATE UNIQUE INDEX unique_atoms on atoms(collection, client, client_index)');
+  });
+}
 
 app.use(require('cookie-parser')());
 app.use(require('body-parser').text());
@@ -39,18 +39,36 @@ function insertValuesForData(these, client_id) {
   return values;
 }
 
+function insert(records, client_id, cb) {
+  let placeholders = new Array(records.length).fill('(?,?,?,?)').join(',');
+  let each = records.map(d => [d.collection, client_id, d.client_index, JSON.stringify(d.value)]);
+  let all = [].concat(...each);
+  if (cb) {
+    db.run('INSERT OR IGNORE into atoms (collection, client, client_index, value) VALUES ' + placeholders, all, cb);
+  } else {
+    db.run('INSERT OR IGNORE into atoms (collection, client, client_index, value) VALUES ' + placeholders, all);
+  }
+}
+
 io.on('connection', function(socket){
   if (!socket.request.headers.cookie) {
-    console.log('no cookie!!!');
+    console.log('no auth cookie!!!');
   } else {
     socket.client_id = cookie.parse(socket.request.headers.cookie).live_id;
     console.log('authed', socket.client_id);
   }
 
-  socket.on('ask', function(next) {
-    db.all('SELECT server_index, client, client_index, value from atoms where client != ? and server_index >= ?', socket.client_id, next, function(err, data) {
+  socket.on('ask', function(collections, next) {
+    console.log(socket.client_id, 'askin bout', collections);
+    let subscriptions = Object.keys(collections);
+    subscriptions.forEach(s => socket.join(s));
+
+    let AFTER = new Array(subscriptions.length).fill('(collection = ? AND server_index >= ?)').join(' OR ');
+    let values = Object.entries(collections).reduce((a, b) => a.concat(b), []);
+
+    db.all('SELECT collection, server_index, client, client_index, value FROM atoms WHERE client != ? AND ('+AFTER+')', socket.client_id, ...values, function(err, data) {
       if (err) { throw err; }
-      console.log(`asked about messages since/including ${next}, found ${data.length}`);
+      console.log(`Found ${data.length} since`, collections);
       data.forEach(d => d.value = JSON.parse(d.value));
       socket.emit('tell', data);
     });
@@ -61,36 +79,36 @@ io.on('connection', function(socket){
       if (data.length === 0) { return; }
       console.log('heard tell', data);
       let records = data.slice();
-      while (records.length > 333) {
-        let these = records.splice(0,333);
-        db.run('INSERT OR IGNORE into atoms (client, client_index, value) VALUES ' + new Array(333).fill('(?,?,?)').join(','), insertValuesForData(these, socket.client_id));
+      while (records.length > 249) {
+        insert(records.splice(0,249), socket.client_id);
       }
-      db.run('INSERT OR IGNORE into atoms (client, client_index, value) VALUES ' + new Array(records.length).fill('(?,?,?)').join(','), insertValuesForData(records, socket.client_id));
-      let min_id = data.reduce((a,d)=>Math.min(a, d.client_index), data[0].client_index);
-      console.log('min id from data', min_id);
-      db.all('SELECT server_index, client, client_index, value FROM atoms WHERE client = ? AND client_index >= ?', socket.client_id, min_id, function(err, results) {
+      insert(records, socket.client_id, function(err) {
         if (err) { throw err; }
         ack();
-        results.forEach(d => d.value = JSON.parse(d.value));
-        console.log('persisted, ready for broadcast', results);
-        for (let s of Object.values(io.sockets.connected)) {
-          console.log('considered',s.client_id,'for broadcast');
-          if (s === socket) {
-            continue;
+        let clock_base = {};
+        for (let item of data) {
+          if (clock_base[item.collection]) {
+            clock_base[item.collection] = Math.min(item.client_index, clock_base[item.collection]);
+          } else {
+            clock_base[item.collection] = item.client_index;
           }
-          console.log('telling', s.client_id, results);
-          s.emit('tell', results);
+        }
+
+        for (let collection in clock_base) {
+          db.all('SELECT server_index, collection, client, client_index, value FROM atoms WHERE client = ? AND collection = ? AND client_index >= ?', socket.client_id, collection, clock_base[collection], function(err, results) {
+            if (err) { throw err; }
+            results.forEach(d => d.value = JSON.parse(d.value));
+            socket.in(collection).emit('tell', results);
+          });
         }
       });
     });
   });
 });
 
-/*
 setInterval(function() {
   console.log('connected clients', Object.values(io.sockets.connected).map(s => s.client_id));
-}, 10000);
-*/
+}, 5000);
 
 http.listen(3001, function(){
   console.log('listening on *:3001');
